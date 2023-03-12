9bb140dd1db7d6e8cf406ad2f9ce7a73ee59a987
Роман <pomo4444ka@gmail.com>
2023-03-12 15:37:49 +0300

    Мне кажется, вот так

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        Author theFirstAuthor = new Author("Лев", "Толстой");
+        Author theSecondAuthor = new Author("Михаил", "Булгаков");
 
-        Book theFirstBook = new Book("Война и мир", "Лев толстой", 1880);
-        Book theSecondBook = new Book("Мастер и Маргарита", "Михаил Булгаков", 1966);
+        Book theFirstBook = new Book("Война и мир", 1880);
+        Book theSecondBook = new Book("Мастер и Маргарита", 1960);
 
-        Author theFirstAuthor = new Author("Иван", "Тургенев");
-        Author theSecondAuthor = new Author("Александр", "Пушкин");
-
-        System.out.println("theFirstBook.nameBook = " + theFirstBook.getNameBook());
-        System.out.println("theFirstBook.nameAuthor = " + theFirstBook.getNameAuthor());
-        System.out.println("theFirstBook.publishingYear = " + theFirstBook.getPublishingYear());
-        theFirstBook.setPublishingYear(1885);
-        System.out.println();
-        System.out.println("theSecondBook = " + theSecondBook.getNameBook());
-        System.out.println("theSecondBook = " + theSecondBook.getNameAuthor());
-        System.out.println("theSecondBook = " + theSecondBook.getPublishingYear());
-        System.out.println();
         System.out.println("theFirstAuthor = " + theFirstAuthor.getTheFirstNameAuthor());
         System.out.println("theFirstAuthor = " + theFirstAuthor.getTheSecondNameAuthor());
+        System.out.println("theFirstBook = " + theFirstBook.getNameBook());
+        System.out.println("theFirstBook = " + theFirstBook.getPublishingYear());
+        theFirstBook.setPublishingYear(1885);
+        System.out.println("theFirstBook = " + theFirstBook.getPublishingYear());
         System.out.println();
         System.out.println("theSecondAuthor = " + theSecondAuthor.getTheFirstNameAuthor());
         System.out.println("theSecondAuthor = " + theSecondAuthor.getTheSecondNameAuthor());
+        System.out.println("theSecondBook = " + theSecondBook.getNameBook());
+        System.out.println("theSecondBook = " + theSecondBook.getPublishingYear());
     }
 
 
